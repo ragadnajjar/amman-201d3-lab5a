333b7b4a01241afcaf65f3112b8ced2eb330ca27
ragadnajjar <ragadnajjar9@gmail.com>
2020-02-21 01:48:45 +0200

    q2

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    var q2=a*b
+return [ q2 ,`The product of ${a} and ${b} is ${q2}.`];
 }
+
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
